7db6ae1e74c45fb11f789eecca29fdf121726ac7
grian32 <grianguy32@gmail.com>
2025-09-21 21:46:32 +0300

    feat: initial gl window for testing

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/go-gl/gl/v3.3-core/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	if err := glfw.Init(); err != nil {
+		log.Fatalln(err)
+	}
+	defer glfw.Terminate()
+
+	glfw.WindowHint(glfw.ContextVersionMajor, 3)
+	glfw.WindowHint(glfw.ContextVersionMinor, 3)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+
+	window, err := glfw.CreateWindow(640, 480, "GRender Test", nil, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	window.MakeContextCurrent()
+
+	if err := gl.Init(); err != nil {
+		log.Fatalln(err)
+	}
+
+	for !window.ShouldClose() {
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
 }
