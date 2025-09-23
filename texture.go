a363c0d449e4c96bb5a1338cfe4470a059faff2a
grian32 <grianguy32@gmail.com>
2025-09-24 00:12:10 +0300

    feat: NewTextureB function

--- texture.go
+++ texture.go
@@ -1,6 +1,7 @@
 package grender
 
 import (
+	"bytes"
 	"fmt"
 	"image"
 	"image/draw"
@@ -15,6 +16,7 @@ type Texture struct {
 	AtlasPos RectP
 }
 
+// NewTexture accepts a path to a .jxl file and creates a texture
 func NewTexture(jxlPath string) (*Texture, error) {
 	tex := &Texture{
 		Size:     Vec2I{},
@@ -28,6 +30,37 @@ func NewTexture(jxlPath string) (*Texture, error) {
 	defer f.Close()
 
 	jxl, err := jpegxl.Decode(f)
+	if err != nil {
+		return nil, err
+	}
+
+	switch img := jxl.(type) {
+	case *image.NRGBA:
+		tex.Image = img
+	case *image.NRGBA64:
+		nrgba := image.NewNRGBA(img.Bounds())
+		draw.Draw(nrgba, nrgba.Bounds(), img, img.Bounds().Min, draw.Src)
+		tex.Image = nrgba
+	default:
+		return nil, fmt.Errorf("unexpected img %T", img)
+	}
+
+	tex.Size.X = int32(jxl.Bounds().Dx())
+	tex.Size.Y = int32(jxl.Bounds().Dy())
+
+	return tex, nil
+}
+
+// NewTextureB accepts a []byte of a jxl file and creates a texture
+func NewTextureB(b []byte) (*Texture, error) {
+	tex := &Texture{
+		Size:     Vec2I{},
+		AtlasPos: RectP{},
+	}
+
+	r := bytes.NewReader(b)
+
+	jxl, err := jpegxl.Decode(r)
 	if err != nil {
 		return nil, err
 	}
